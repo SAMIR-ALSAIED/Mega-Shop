80cf94098e50e63a1f7d3e70f7517f8c5857b137
samiralsaied <165153491+SAMIR-ALSAIED@users.noreply.github.com>
2024-04-12 16:08:04 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+// var header=document.getElementById("header");
+// var navbar=document.getElementById("navbar");
+
+
+// window .onscroll =function(){
+
+// if (window.scrollY >=100){
+    
+//     header.style.backgroundColor='#000000';
+//     header.style.color='#fff';
+//     navbar.style.color=' #ff887';
+
+
+// }
+// else if( window.scrollY ==0){
+
+//     header.style.backgroundColor='#fff';
+//     header.style.color='#fff';
+//     aa.style.color=' #fa5252';
+ 
+   
+// }
+
+
+    
+// }
